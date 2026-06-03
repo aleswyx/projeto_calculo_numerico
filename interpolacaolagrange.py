2d299b32867005ff8bf6102b55bb579e7a75b74e
Alessandro França Ramos <111668066+aleswyx@users.noreply.github.com>
2026-06-02 21:52:26 -0300

    interpolação lagrange

---
+++ interpolacaolagrange.py
@@ -0,0 +1,13 @@
+2. Interpolação de Lagrange
+
+Python
+def interpolacao_lagrange(x_pontos, y_pontos, x_alvo):
+    n = len(x_pontos)
+    resultado = 0.0
+    for i in range(n):
+        termo = y_pontos[i]
+        for j in range(n):
+            if i != j:
+                termo *= (x_alvo - x_pontos[j]) / (x_pontos[i] - x_pontos[j])
+        resultado += termo
+    return resultado
