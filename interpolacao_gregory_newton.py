d1321923f89cd72ae7f61b80881f80ed6ffbce03
Alessandro França Ramos <111668066+aleswyx@users.noreply.github.com>
2026-06-02 22:01:01 -0300

    interpolacao_gregory_newton.py

---
+++ interpolacao_gregory_newton.py
@@ -0,0 +1,25 @@
+4. Gregory-Newton (Para pontos equidistantes)
+Arquivo sugerido: interpolacao/gregory_newton.py
+
+Python
+def fatorial(n):
+    res = 1
+    for i in range(2, n + 1): res *= i
+    return res
+
+def interpolacao_gregory_newton(x_pontos, y_pontos, x_alvo):
+    n = len(x_pontos)
+    h = x_pontos[1] - x_pontos[0]
+    tabela = [[0.0] * n for _ in range(n)]
+    for i in range(n): tabela[i][0] = y_pontos[i]
+    
+    for j in range(1, n):
+        for i in range(n - j):
+            tabela[i][j] = tabela[i+1][j-1] - tabela[i][j-1]
+            
+    resultado = y_pontos[0]
+    produto = 1.0
+    for i in range(1, n):
+        produto *= (x_alvo - x_pontos[i-1])
+        resultado += (tabela[0][i] / (fatorial(i) * (h**i))) * produto
+    return resultado
