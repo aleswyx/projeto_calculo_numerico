ea98f6fbdb1651753f860079b94208b783434d09
Alessandro França Ramos <111668066+aleswyx@users.noreply.github.com>
2026-06-28 17:43:11 -0300

    Add initial parameters for CPU simulation

--- Unidade 3/simulacao_cpu.py
+++ Unidade 3/simulacao_cpu.py
@@ -1,1 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# -----------------------------------------------------------------------------
+# 1. PARÂMETROS DO PROBLEMA REAL (PVI)
+# -----------------------------------------------------------------------------
+T0 = 90.0          # Temperatura inicial da CPU (°C)
+T_amb = 25.0       # Temperatura ambiente (°C)
+k = 0.01           # Constante de resfriamento do cooler (s^-1)
+tol = 0.1          # Tolerância de proximidade térmica (€) para o critério de parada
+h = 10.0           # Passo de tempo (em segundos) - mude para testar a estabilidade!
 
